a5364d3d34dbb5d1b33c1ac2bbd9f4d59f47acc2
Era <123576722+Eraaaaaaaaaa@users.noreply.github.com>
2025-01-28 18:55:17 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -91,6 +91,32 @@ app.post('/users', (req, res) => {
     res.status(201).send({ id: 2, name });
 });
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Përditëso një përdorues
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID e përdoruesit
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Përdoruesi u përditësua
+ */
+
 // Update per user
 app.put('/users/:id', (req, res) => {
     const { id } = req.params;
@@ -98,11 +124,146 @@ app.put('/users/:id', (req, res) => {
     res.send({ id, name });
   });
 
+  /**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Fshij një përdorues
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID e përdoruesit
+ *     responses:
+ *       200:
+ *         description: Përdoruesi u fshi
+ */
+
 // Delete per user
   app.delete('/users/:id', (req, res) => {
     const { id } = req.params;
     res.send({ message: `Përdoruesi me id ${id} u fshi.` });
   });
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Merr të gjithë produktet
+ *     responses:
+ *       200:
+ *         description: Lista e produkteve
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ * 
+ */
+
+
+// Geti per products 
+app.get('/products', (req, res) => {
+  res.send([{ id: 1, name: 'Lapsi' }]);
+});
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Shto një produkt të ri
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Produkti u shtua
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ */
+
+// Post per products
+app.post('/products', (req, res) => {
+  const { name } = req.body;
+  res.status(201).send({ id: 2, name });
+});
+
+  /**
+ * @swagger
+ * /products/{id}:
+ *   put:
+ *     summary: Përditëso një produkt
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID e produktit
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Produkti u përditësua
+ */
+
+  // Update per products
+app.put('/products/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  res.send({ id, name });
+});
+
+  /**
+ * @swagger
+ * /products/{id}:
+ *   delete:
+ *     summary: Fshij një produkt
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID e produktit
+ *     responses:
+ *       200:
+ *         description: Produkti u fshi
+ */
+
+// Delete per products
+app.delete('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.send({ message: `Produkti me id ${id} u fshi.` });
+});
   
 
 // Start the Server
